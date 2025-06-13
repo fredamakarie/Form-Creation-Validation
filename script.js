aee9d1c4af68cea9a410bbb7cef9bc3cda857214
Omonye Omonlumhen <omon.omonye@outlook.com>
2025-06-13 14:10:59 +0100

    Form creation and validation

---
+++ script.js
@@ -0,0 +1,46 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+  const form = document.getElementById("registration-form");
+  const feedbackDiv = document.getElementById("form-feedback");
+
+form.addEventListener("submit", function(event) {
+  event.preventDefault();
+
+const username = document.getElementById("username").value.trim();
+const password = document.getElementById("password").value.trim();
+const email = document.getElementById("email").value.trim();
+
+let isValid = true;
+let messages = [];
+
+if (username.length < 3) {
+    isValid = false
+messages.push ("Username must be at least 3 characters")
+};
+
+if (!email.includes ("@")|| !email. includes (".")){
+isValid = false
+messages.push ("Please enter your full email address")
+}
+
+if (password.length < 8){
+isValid = false
+messages.push ("Password must be at least 8 characters")
+}
+
+ feedbackDiv.style.display = "block";
+
+    if (isValid) {
+      feedbackDiv.textContent = "Registration successful!";
+      feedbackDiv.style.color = "#28a745"; // green
+    } else {
+      feedbackDiv.innerHTML = messages.join("<br>");
+      feedbackDiv.style.color = "#dc3545"; // red
+    }
+
+
+
+});
+});
+
+
